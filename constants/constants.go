fb1cadaa297e20caf30d5a8ff65c14f9105fe661
ivyzhang333 <99704583+ivyzhang333@users.noreply.github.com>
2022-03-24 11:56:29 -0700

    uploading WeLink backend

---
+++ constants/constants.go
@@ -0,0 +1,10 @@
+package constants
+
+const (
+    USER_INDEX = "user"
+    POST_INDEX = "post"
+    ES_URL = "http://10.138.0.2:9200"
+    ES_USERNAME = "ivyzhang333"
+    ES_PASSWORD = "LaiOfferWeiZhang666"
+    GCS_BUCKET = "weizhang-bucket-333"
+)
